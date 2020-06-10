fe2565a929aee7bdbd040af57da7d5af7689dbef
Andy Wang <aw1912@ic.ac.uk>
2020-06-10 12:25:08 +0100

    adding factory functions for wall and ball

--- src/robox2d/common.hpp
+++ src/robox2d/common.hpp
@@ -15,6 +15,10 @@ namespace robox2d {
 
     b2Body* createBody( std::shared_ptr<b2World> world, const b2Vec2& halfSize, const b2BodyType type, const b2Vec3& transformation, const float density = 1.0f);
 
+    b2Body* createBall( std::shared_ptr<b2World> world, float radius, const b2BodyType type, const b2Vec2& position, const float density = 1.0f);
+
+    b2Body* createWall( std::shared_ptr<b2World> world, const b2Vec2& halfSize, const b2Vec2& position);
+
     b2RevoluteJoint* createServo( std::shared_ptr<b2World> world, b2Body* bodyA, b2Body* bodyB,  const b2Vec2 & anchor);
 
 
