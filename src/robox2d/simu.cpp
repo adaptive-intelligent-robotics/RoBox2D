1fc65dd13897d3abc72429e835699534a816c647
Andy Wang <aw1912@ic.ac.uk>
2020-06-15 12:18:28 +0100

    changes to simu

--- src/robox2d/simu.cpp
+++ src/robox2d/simu.cpp
@@ -1,100 +1,100 @@
 #include "simu.hpp"
 #include <iostream>
 #include <boost/math/common_factor.hpp>
-namespace robox2d {
-  
-  Simu::Simu(size_t physic_freq, size_t control_freq, size_t graphic_freq) :
-    _world(new b2World(b2Vec2(0.0f, 0.0f))),
-    _time(0),
-    _graphics(nullptr)
-  {
-  
-    _time_step = 1.0f/double( boost::math::lcm( boost::math::lcm(physic_freq, control_freq) , graphic_freq ));
-    _physic_period = 1.0f/(double)physic_freq;
-    _control_period = 1.0f/(double)control_freq;
-    _graphic_period = 1.0f/(double)graphic_freq;
-  }
-  
-  Simu::~Simu()
-  {
-    _robots.clear();
-    //_descriptors.clear();
-    //_cameras.clear();
-  }
-  
-  void Simu::run(double max_duration, std::array<Eigen::VectorXf, 2> &trajectories, Eigen::VectorXf &full_trajectory, int trajectory_length)
-  {
-    int record_freq{static_cast<int>(max_duration / _time_step / trajectory_length)};
-    int trajectory_frame_counter{0};
-    int loop_counter{1};
+namespace robox2d
+{
 
-    while ((_time - max_duration) < -_time_step/2.0 && (!_graphics || !_graphics->done())) 
+    Simu::Simu(size_t physic_freq, size_t control_freq, size_t graphic_freq) : _world(new b2World(b2Vec2(0.0f, 0.0f))),
+                                                                               _time(0),
+                                                                               _graphics(nullptr)
     {
-      _time+=_time_step;
 
-      // control step
-      if(std::abs(std::remainder(_time,_control_period)) < 1e-4)
-      {
-        for (auto& robot : _robots)
-          robot->control_update(_time);
-      }
-      
-      // physic step
-      if(std::abs(std::remainder(_time,_physic_period)) < 1e-4)
-	    {
-      for (auto& robot : _robots)
-        robot->physic_update();	
-
-      _world->Step(_time_step, velocityIterations, positionIterations);
-	    }
-
-      // graphic step
-      if(_graphics && std::abs(std::remainder(_time,_graphic_period)) < 1e-4)
-      {
-        _graphics->refresh();
-        usleep(_graphic_period *1e6);
-      }
-
-      // first ball retrieved is actual ball
-      bool is_first_ball{true};
-
-      // two trajectories, one actual and one potential fake
-      int trajectory_index{0};
-
-      for(b2Body* body = _world->GetBodyList(); body; body = body->GetNext())
-      {
-        if (body->GetFixtureList()->GetShape()->GetType() == 0)
-        {
-          if (is_first_ball)
-          {
-            b2Vec2 body_pos = body->GetWorldCenter();
-            full_trajectory[(loop_counter - 1) * 2] = body_pos.x;
-            full_trajectory[(loop_counter - 1) * 2 + 1] = body_pos.y;
-            is_first_ball = false;
-          }
-          if (loop_counter % record_freq == 0)
-          {
-            b2Vec2 body_pos = body->GetWorldCenter();
-            trajectories[trajectory_index][trajectory_frame_counter] = body_pos.x;
-            trajectories[trajectory_index][trajectory_frame_counter + 1] = body_pos.y;
-            ++trajectory_index;
-          }
-        }
-      }
-      if (loop_counter % record_freq == 0)
-        trajectory_frame_counter += 2;
-
-      ++loop_counter;
+        _time_step = 1.0f / double(boost::math::lcm(boost::math::lcm(physic_freq, control_freq), graphic_freq));
+        _physic_period = 1.0f / (double)physic_freq;
+        _control_period = 1.0f / (double)control_freq;
+        _graphic_period = 1.0f / (double)graphic_freq;
     }
-  }
 
-  std::shared_ptr<gui::Base> Simu::graphics() const { return _graphics; }
-  
-  void Simu::set_graphics(const std::shared_ptr<gui::Base>& graphics) { _graphics = graphics; }
-  
-  std::shared_ptr<b2World> Simu::world()  {return _world; }
-    
-  /*
+    Simu::~Simu()
+    {
+        _robots.clear();
+        //_descriptors.clear();
+        //_cameras.clear();
+    }
+
+    void Simu::run(double max_duration, std::array<Eigen::VectorXf, 2> &trajectories, Eigen::VectorXf &full_trajectory, int trajectory_length)
+    {
+        int record_freq{static_cast<int>(max_duration / _time_step / trajectory_length)};
+        int trajectory_frame_counter{0};
+        int loop_counter{1};
+
+        while ((_time - max_duration) < -_time_step / 2.0 && (!_graphics || !_graphics->done()))
+        {
+            _time += _time_step;
+
+            // control step
+            if (std::abs(std::remainder(_time, _control_period)) < 1e-4)
+            {
+                for (auto &robot : _robots)
+                    robot->control_update(_time);
+            }
+
+            // physic step
+            if (std::abs(std::remainder(_time, _physic_period)) < 1e-4)
+            {
+                for (auto &robot : _robots)
+                    robot->physic_update();
+
+                _world->Step(_time_step, velocityIterations, positionIterations);
+            }
+
+            // graphic step
+            if (_graphics && std::abs(std::remainder(_time, _graphic_period)) < 1e-4)
+            {
+                _graphics->refresh();
+                usleep(_graphic_period * 1e6);
+            }
+
+            // first ball retrieved is actual ball
+            bool is_first_ball{true};
+
+            // two trajectories, one actual and one potential fake
+            int trajectory_index{0};
+
+            for (b2Body *body = _world->GetBodyList(); body; body = body->GetNext())
+            {
+                if (body->GetFixtureList()->GetShape()->GetType() == 0)
+                {
+                    if (is_first_ball)
+                    {
+                        b2Vec2 body_pos = body->GetWorldCenter();
+                        full_trajectory[(loop_counter - 1) * 2] = body_pos.x;
+                        full_trajectory[(loop_counter - 1) * 2 + 1] = body_pos.y;
+                        is_first_ball = false;
+                    }
+                    if (loop_counter % record_freq == 0)
+                    {
+                        b2Vec2 body_pos = body->GetWorldCenter();
+                        trajectories[trajectory_index][trajectory_frame_counter] = body_pos.x;
+                        trajectories[trajectory_index][trajectory_frame_counter + 1] = body_pos.y;
+                        ++trajectory_index;
+                    }
+                }
+            }
+            if (loop_counter % record_freq == 0)
+                trajectory_frame_counter += 2;
+
+            ++loop_counter;
+        }
+    }
+
+    std::shared_ptr<gui::Base> Simu::graphics() const { return _graphics; }
+
+    void Simu::set_graphics(const std::shared_ptr<gui::Base> &graphics) { _graphics = graphics; }
+
+    std::shared_ptr<b2World> Simu::world() { return _world; }
+
+    /*
   void Simu::add_descriptor(const std::shared_ptr<descriptor::BaseDescriptor>& desc)
   {
     _descriptors.push_back(desc);
@@ -112,50 +112,49 @@ namespace robox2d {
     }
   */
 
+    size_t Simu::num_robots() const { return _robots.size(); }
 
-  size_t Simu::num_robots() const { return _robots.size(); }
+    std::vector<std::shared_ptr<Robot>> Simu::robots() const { return _robots; }
 
-  std::vector<std::shared_ptr<Robot>> Simu::robots() const { return _robots; }
-
-  std::shared_ptr<Robot> Simu::robot(size_t index) const
-  {
-    //ROBOT_DART_ASSERT(index < _robots.size(), "Robot index out of bounds", nullptr);
-    return _robots[index];
-  }
-
-  void Simu::add_robot(const std::shared_ptr<Robot>& robot)
-  {
-    
-    _robots.push_back(robot);
-    //_world->addSkeleton(robot->skeleton());
-    
-  }
-  
-  void Simu::remove_robot(const std::shared_ptr<Robot>& robot)
-  {
-    auto it = std::find(_robots.begin(), _robots.end(), robot);
-    if (it != _robots.end()) {
-      //   _world->removeSkeleton(robot->skeleton());
-      _robots.erase(it);
+    std::shared_ptr<Robot> Simu::robot(size_t index) const
+    {
+        //ROBOT_DART_ASSERT(index < _robots.size(), "Robot index out of bounds", nullptr);
+        return _robots[index];
     }
-  }
-  
-  void Simu::remove_robot(size_t index)
-  {
-    //ROBOT_DART_ASSERT(index < _robots.size(), "Robot index out of bounds", );
-    //_world->removeSkeleton(_robots[index]->skeleton());
-    _robots.erase(_robots.begin() + index);
-  }
-  
-  void Simu::clear_robots()
-  {
-    //for (auto& robot : _robots) {
-    //     _world->removeSkeleton(robot->skeleton());
-    // }
-    _robots.clear();
-  }
 
-  /*void Simu::remove_descriptor(const std::shared_ptr<descriptor::BaseDescriptor>& desc)
+    void Simu::add_robot(const std::shared_ptr<Robot> &robot)
+    {
+
+        _robots.push_back(robot);
+        //_world->addSkeleton(robot->skeleton());
+    }
+
+    void Simu::remove_robot(const std::shared_ptr<Robot> &robot)
+    {
+        auto it = std::find(_robots.begin(), _robots.end(), robot);
+        if (it != _robots.end())
+        {
+            //   _world->removeSkeleton(robot->skeleton());
+            _robots.erase(it);
+        }
+    }
+
+    void Simu::remove_robot(size_t index)
+    {
+        //ROBOT_DART_ASSERT(index < _robots.size(), "Robot index out of bounds", );
+        //_world->removeSkeleton(_robots[index]->skeleton());
+        _robots.erase(_robots.begin() + index);
+    }
+
+    void Simu::clear_robots()
+    {
+        //for (auto& robot : _robots) {
+        //     _world->removeSkeleton(robot->skeleton());
+        // }
+        _robots.clear();
+    }
+
+    /*void Simu::remove_descriptor(const std::shared_ptr<descriptor::BaseDescriptor>& desc)
     {
     auto it = std::find(_descriptors.begin(), _descriptors.end(), desc);
     if (it != _descriptors.end()) {
@@ -176,10 +175,9 @@ namespace robox2d {
 
    */
 
-  void Simu::add_floor()//(double floor_width, double floor_height, const Eigen::Vector6d& pose, const std::string& floor_name)
-  {
-    common::createBox(_world, {50.0f, 0.50f}, b2_staticBody, {0.0f,-10.5f,0.0f} );
-  }
+    void Simu::add_floor() //(double floor_width, double floor_height, const Eigen::Vector6d& pose, const std::string& floor_name)
+    {
+        common::createBox(_world, {50.0f, 0.50f}, b2_staticBody, {0.0f, -10.5f, 0.0f});
+    }
 
-    
-} // namespace robot_dart
+} // namespace robox2d
--- src/robox2d/simu.hpp
+++ src/robox2d/simu.hpp
@@ -1,11 +1,9 @@
 #ifndef ROBOX2D_SIMU_HPP
 #define ROBOX2D_SIMU_HPP
 
-
-#include <algorithm> 
-#include<memory>
-#include<vector>
-
+#include <algorithm>
+#include <memory>
+#include <vector>
 
 #include <box2d/box2d.h>
 
@@ -13,27 +11,25 @@
 #include "robot.hpp"
 #include "gui/base.hpp"
 
-namespace robox2d {
-  class Simu {
-  public:
-    using robot_t = std::shared_ptr<Robot>;
-    
-    Simu(size_t physic_freq=100, size_t control_freq=50, size_t graphic_freq=50);
-    
-    ~Simu();
-    
-    void run(double max_duration, std::array<Eigen::VectorXf, 2> &trajectories, Eigen::VectorXf &full_trajectory, int trajectory_length);
+namespace robox2d
+{
+    class Simu
+    {
+    public:
+        using robot_t = std::shared_ptr<Robot>;
 
-    std::shared_ptr<gui::Base> graphics() const;
-    void set_graphics(const std::shared_ptr<gui::Base>& graphics);
-    
-    std::shared_ptr<b2World> world();
+        Simu(size_t physic_freq = 100, size_t control_freq = 50, size_t graphic_freq = 50);
 
+        ~Simu();
 
+        void run(double max_duration, std::array<Eigen::VectorXf, 2> &trajectories, Eigen::VectorXf &full_trajectory, int trajectory_length);
 
-    
-    
-    /*template <typename Descriptor>
+        std::shared_ptr<gui::Base> graphics() const;
+        void set_graphics(const std::shared_ptr<gui::Base> &graphics);
+
+        std::shared_ptr<b2World> world();
+
+        /*template <typename Descriptor>
     void add_descriptor(size_t desc_dump = 1)
     {
       add_descriptor(std::make_shared<Descriptor>(Descriptor{*this, desc_dump}));
@@ -47,8 +43,8 @@ namespace robox2d {
         void remove_descriptor(size_t index);
         void clear_descriptors();
     */
-    
-    /*    void add_camera(const std::shared_ptr<gui::Base>& cam);
+
+        /*    void add_camera(const std::shared_ptr<gui::Base>& cam);
         std::vector<std::shared_ptr<gui::Base>> cameras() const;
         std::shared_ptr<gui::Base> camera(size_t index) const;
 
@@ -56,37 +52,35 @@ namespace robox2d {
         void remove_camera(size_t index);
         void clear_cameras();
     */
-    
-    
-    size_t num_robots() const;
-    std::vector<robot_t> robots() const;
-    robot_t robot(size_t index) const;
-    
-    void add_robot(const robot_t& robot);
-    void remove_robot(const robot_t& robot);
-    void remove_robot(size_t index);
-    void clear_robots();
-    
-    
-    void add_floor();//double floor_width = 10.0, double floor_height = 0.1, const Eigen::Vector6d& pose = Eigen::Vector6d::Zero(), const std::string& floor_name = "floor");
-    //  void add_checkerboard_floor(double floor_width = 10.0, double floor_height = 0.1, double size = 1., const Eigen::Vector6d& pose = Eigen::Vector6d::Zero(), const std::string& floor_name = "checkerboard_floor");
 
-  protected:
-    std::shared_ptr<b2World> _world;
-    double _physic_period;
-    double _control_period;
-    double _graphic_period;
-    double _time;
-    double _time_step;
-    
-    int32 velocityIterations = 6;
-    int32 positionIterations = 2;
-     
-    //std::vector<std::shared_ptr<descriptor::BaseDescriptor>> _descriptors;
-    //std::vector<std::shared_ptr<gui::Base>> _cameras; // designed to include mainly graphcis::CameraOSR
-    std::vector<robot_t> _robots;
-    std::shared_ptr<gui::Base> _graphics;
-  };
-} // namespace robot_dart
+        size_t num_robots() const;
+        std::vector<robot_t> robots() const;
+        robot_t robot(size_t index) const;
+
+        void add_robot(const robot_t &robot);
+        void remove_robot(const robot_t &robot);
+        void remove_robot(size_t index);
+        void clear_robots();
+
+        void add_floor(); //double floor_width = 10.0, double floor_height = 0.1, const Eigen::Vector6d& pose = Eigen::Vector6d::Zero(), const std::string& floor_name = "floor");
+                          //  void add_checkerboard_floor(double floor_width = 10.0, double floor_height = 0.1, double size = 1., const Eigen::Vector6d& pose = Eigen::Vector6d::Zero(), const std::string& floor_name = "checkerboard_floor");
+
+    protected:
+        std::shared_ptr<b2World> _world;
+        double _physic_period;
+        double _control_period;
+        double _graphic_period;
+        double _time;
+        double _time_step;
+
+        int32 velocityIterations = 6;
+        int32 positionIterations = 2;
+
+        //std::vector<std::shared_ptr<descriptor::BaseDescriptor>> _descriptors;
+        //std::vector<std::shared_ptr<gui::Base>> _cameras; // designed to include mainly graphcis::CameraOSR
+        std::vector<robot_t> _robots;
+        std::shared_ptr<gui::Base> _graphics;
+    };
+} // namespace robox2d
 
 #endif
